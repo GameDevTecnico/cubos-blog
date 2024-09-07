6f9c98339f9f70359f813ac326e68172c68413e2
Ricardo Antunes <me@riscadoa.com>
2024-09-07 08:55:13 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # Flake used for development with nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
